0fb04abe7482715a12561ab3639b31f6651ba4ad
Essence Roberts <essencenroberts@gmail.com>
2026-08-02 22:32:02 -0500

    select DOM elements

---
+++ script.js
@@ -0,0 +1,27 @@
+// Select DOM elements & connect JavaScript to HTML 
+const form = document.getElementById('registrationForm')
+
+const usernameInput = document.getElementById('username');
+
+const emailInput = document.getElementById('email');
+
+const passwordInput = document.getElementById('password');
+
+const usernameError = document.getElementById('emailError');
+
+const emailError = document.getElementById('emailError');
+
+const passwordError = document.getElementById('passwordError');
+
+const confirmPasswordError = document.getElementById('confirmPasswordError');
+
+
+// load data
+
+//functions + error messages 
+
+// function validation
+
+// add eventListner
+
+// form submission
