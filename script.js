00c15873b1050533fb59eae665d7d924fbf237c7
Essence Roberts <essencenroberts@gmail.com>
2026-08-02 22:48:55 -0500

    add event listeners

--- script.js
+++ script.js
@@ -17,11 +17,96 @@ const confirmPasswordError = document.getElementById('confirmPasswordError');
 
 
 // load data
+window.addEventListener('DOMContentLoaded', () => {
+  const savedUsername = localStorage.getItem('username');
+  if (savedUsername) {
+    usernameInput.value = savedUsername
+  }
+});
+
 
 //functions + error messages 
+  function getErrorMessage(input) {
+    const validity = input.validity;
+
+    if (validity.valid) return '';
+
+    if (validity.valueMissing) {
+      return 'This field is required.';
+    }
+
+    if (input.id === 'username' && validity.tooShort) {
+      return `Username must be at least ${input.minLength} characters long.`;
+    }
+
+    if (input.id === 'email' && validity.typeMismatch) {
+      return 'Please enter a valid email address.';
+    }
+
+    if (input.id === 'passwprd' && (validity.patternMismatch || valid.tooShort)) {
+      return 'Password must be at least 8 chracters and include an uppercase letter, a lowercase letter, and a number.';
+    }
+
+    return input.validationMessage
+  }
 
 // function validation
+function validateField(input, errorSpan) {
+  // confirm password needs a manual match check
+  if (input.id === 
+  'confirmPassword') {
+    return validateConfirmPassword();
+  }
+
+  const message = getErrorMessage(input);
+
+  if (message) {
+    errorSpan.textContent = message;
+    return false;
+  } else {
+    errorSpan.textContent = '';
+    return true;
+  }
+}
+
+function validateConfirmPassword(){
+  if (confirmPasswordInput.validity.valueMissing) {
+    confirmPasswordError.textContent = 'Please confirm your password.';
+    return false;
+  }
+
+
+if (confirmPasswordInput.value !== passwordInput.value) {
+  confirmPasswordError.textContent = 'Passwords do not match.'; 
+  
+  return false;
+}
+
+confirmPasswordError.textContent = '';
+return true;
+
+}
 
 // add eventListner
+usernameInput.addEventListener('input', () => {
+  validateField(usernameInput, usernameError);
+});
+
+emailInput.addEventListener('input', () => {
+  validateField(emailInput, emailError);
+});
+
+passwordInput.addEventListener('input', () => {
+  validateField(passwordInput, passwordError);
+
+  if (confirmPasswordInput.value) {
+    validateConfirmPassword();
+  }
+});
+
+confirmPasswordInput.addEventListener('input', () => {
+  validateConfirmPassword();
+});
+
 
 // form submission
